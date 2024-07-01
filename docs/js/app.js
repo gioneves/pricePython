bbb9ed8bafa4ee979b22dc9f5ea7382aabc91dc4
gioneves <gioneves@outlook.com>
2024-07-01 19:33:19 -0300

    first commit

--- docs/js/app.js
+++ docs/js/app.js
@@ -1,63 +1,137 @@
+// shinySetInputValue
+
+// buttons
+document.addEventListener("DOMContentLoaded", function() {
+  function shinySetFunction() {
+    $("[id^='setBttn']").on("click", function() {
+      Shiny.setInputValue(this.id, Math.random(), {priority: "event"});
+    });
+  };
+  shinySetFunction();
+});
+
 // change theme
 $(document).ready(function() {
   $('#darkTheme').on('click', function() {
     const clique = $(this).data('clique');
     if (clique) {
-      $('body, [id^="dropdown"]').animate({
+      $('body, [id^="dropdown"], .modal-content, [id^="autoDrops"]').animate({
         "background-color": "#fafac0",
         "color": "#242832"
       }, "slow");
+      $('body').on('show.bs.modal', function() {
+        $('.modal-body').css({
+          'background-color': '#fafac0',
+          'color': '#242832'
+        });
+      });
     } else {
-      $('body, [id^="dropdown"]').animate({
+      $('body, [id^="dropdown"], .modal-content, [id^="autoDrops"]').animate({
         "background-color": "#242832",
         "color": "#fafac0"
       }, "slow");
+      $('body').on('show.bs.modal', function() {
+        $('.modal-body').css({
+          'background-color': '#242832',
+          'color': '#fafac0'
+        });
+      });
     }
     $(this).data('clique', !clique);
   });
 });
 
-// Inicializar i18next
+// i18next
 i18next.use(i18nextBrowserLanguageDetector).init({
-    fallbackLng: 'en', // Linguagem padrão
-    debug: true,
-    resources: {
-        en: {
-          translation: {
-            "tChangeTheme": "Change theme"
-          }
-        },
-        es: {
-          translation: {
-            "tChangeTheme": "Hola Mundo"
-          }
-        },
-        fr: {
-          translation: {
-            "tChangeTheme": "Bonjour le monde"
-          }
-        }
+  fallbackLng: 'en', // pattern language
+  debug: true,
+  resources: {
+    en: {
+      translation: {
+        "tChangeTheme": "Change theme",
+        "tIntroGuide": "Analysis guide",
+        "tHowEarn": "HOW MUCH DO YOU EARN?",
+        "tHowMuch": "HOW MUCH DO YOU SELL FOR?",
+        "tHowQuant": "(AVERAGE) QUANTITY SOLD"
+      }
+    },
+    es: {
+      translation: {
+        "tChangeTheme": "Cambiar tema",
+        "tIntroGuide": "Guía de análisis",
+        "tHowEarn": "¿POR CUÁNTO SE VENDE?",
+        "tHowMuch": "¿CUÁNTO GANAS POR CADA VENTA?",
+        "tHowQuant": "CANTIDAD (MEDIA) VENDIDA"
+      }
+    },
+    ch: {
+      translation: {
+        "tChangeTheme": "Change theme",
+        "tIntroGuide": "分析指南。",
+        "tHowEarn": "你们卖多少钱？",
+        "tHowMuch": "你收入多少？",
+        "tHowQuant": "平均销售数量。"
+      }
+    },
+    ru: {
+      translation: {
+        "tChangeTheme": "Bonjour le monde",
+        "tIntroGuide": "Руководство по анализу",
+        "tHowEarn": "ЗА СКОЛЬКО ВЫ ПРОДАЕТЕ?",
+        "tHowMuch": "СКОЛЬКО ТЫ ЗАРАБАТЫВАЕШЬ?",
+        "tHowQuant": "(СРЕДНЕЕ) КОЛИЧЕСТВО ПРОДАННОГО"
+      }
+    },
+    jp: {
+      translation: {
+        "tChangeTheme": "Hola Mundo",
+        "tIntroGuide": "分析ガイド。",
+        "tHowEarn": "いくらで売られていますか？",
+        "tHowMuch": "いくら稼いでいますか？",
+        "tHowQuant": "(平均) 販売数量"
+      }
+    },
+    pt: {
+      translation: {
+        "tChangeTheme": "Mudar tema",
+        "tIntroGuide": "Analysis guide",
+        "tHowEarn": "QUANTO VOCÊ GANHA?",
+        "tHowMuch": "POR QUANTO VOCÊ VENDE?",
+        "tHowQuant": "Guia de análise"
+      }
     }
+  }
 }, function(err, t) {
-    // Atualizar o conteúdo da página após a inicialização
-    updateContent();
+  updateContent();
 });
 
-// Função para atualizar o conteúdo da página com as traduções
 function updateContent() {
-    document.querySelectorAll('[data-i18n]').forEach(function(element) {
-        var key = element.getAttribute('data-i18n');
-        element.textContent = i18next.t(key);
-    });
+  document.querySelectorAll('[data-i18n]').forEach(function(element) {
+    var key = element.getAttribute('data-i18n');
+    element.textContent = i18next.t(key);
+  });
 }
 
-// Função para mudar o idioma e inicializar a página
 function changeLanguageAndInit(lng) {
-    // Mudar o idioma
-    i18next.changeLanguage(lng, function() {
-        // Atualizar o conteúdo da página
-        updateContent();
-        // Disparar o clique no botão fakeButtonInitPage
-        document.getElementById('complexTranslation').click();
-    });
+  i18next.changeLanguage(lng, function() {
+    updateContent();
+    document.getElementById('complexTranslation').click();
+  });
 }
+
+// voices (annyang)
+const voiceCommands = {
+  'change': function() {
+    const darkAnny = document.getElementById("darkTheme");
+    darkAnny.click();
+  },
+  'container': function() {
+    const dropOpenAnny = document.getElementById("dropBttn");
+    dropOpenAnny.click();
+  }
+};
+
+if (annyang) {
+  annyang.addCommands(voiceCommands);
+  annyang.start();
+};
--- myapp/app.py
+++ myapp/app.py
@@ -1,48 +1,136 @@
 # require
-from shiny import ui, render, App
+from shiny import App, Inputs, Outputs, Session, reactive, render, ui
 from shinylive import *
 from pathlib import Path
+from PIL import Image
 
-# html (modules)
-headerApp = Path("site/html/header/headerApp.html").read_text()
-dropdownStart = Path("site/html/dropdowns/dropdownStart.html").read_text()
+# html modules
+headerApp = Path("docs/html/header/headerApp.html").read_text()
+dropdownStart = Path("docs/html/dropdowns/dropdownStart.html").read_text()
 
 htmlContent = headerApp + dropdownStart
 
 # css/ js
-css_file = Path("site/css/app.css")
-js_file = Path("site/js/app.js")
+css_file = Path("docs/css/app.css")
+js_file = Path("docs/js/app.js")
+image = Path("docs/images/brazilLang.png")
 
 # ui
 app_ui = ui.page_fluid(
-    # header
-    ui.head_content(
-      ui.tags.title('Hello Shiny!'),
-      ui.tags.script(src='https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.13.3/jquery-ui.min.js'),
-      ui.tags.link(rel='stylesheet', href='https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.13.3/themes/base/jquery-ui.min.css'),
-      ui.tags.link(rel='stylesheet', href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css'),
-      ui.tags.script(src='https://cdnjs.cloudflare.com/ajax/libs/i18next/21.6.6/i18next.min.js'),
-      ui.tags.script(src='https://cdnjs.cloudflare.com/ajax/libs/i18next-browser-languagedetector/6.1.1/i18nextBrowserLanguageDetector.min.js'),
-      ui.tags.link(rel='stylesheet', href='https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css'),
-      ui.tags.link(rel='stylesheet', href='https://cdnjs.cloudflare.com/ajax/libs/bttn.css/0.2.4/bttn.min.css'),
-      ui.tags.script(src='https://cdnjs.cloudflare.com/ajax/libs/echarts/5.5.0/echarts.min.js'),
-      ui.tags.script(src='https://cdn.jsdelivr.net/npm/wnumb@1.2.0/wNumb.min.js'),
-      ui.tags.script(src='https://cdnjs.cloudflare.com/ajax/libs/noUiSlider/14.6.3/nouislider.min.js'),
-      ui.tags.link(rel='stylesheet', href='https://cdnjs.cloudflare.com/ajax/libs/noUiSlider/14.6.3/nouislider.min.css'),
-      ui.include_css(css_file),
-      ui.include_js(js_file)
-    ),
-    # body modules
-    ui.HTML(f"<body>{htmlContent}</body>"),
-    ui.output_ui("txt")
+  # header
+  ui.head_content(
+    ui.tags.title('Hello Shiny!'),
+    ui.tags.script(src='https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.13.3/jquery-ui.min.js'),
+    ui.tags.link(rel='stylesheet', href='https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.13.3/themes/base/jquery-ui.min.css'),
+    ui.tags.link(rel='stylesheet', href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css'),
+    ui.tags.script(src='https://cdnjs.cloudflare.com/ajax/libs/i18next/21.6.6/i18next.min.js'),
+    ui.tags.script(src='https://cdnjs.cloudflare.com/ajax/libs/i18next-browser-languagedetector/6.1.1/i18nextBrowserLanguageDetector.min.js'),
+    ui.tags.script(src='https://cdnjs.cloudflare.com/ajax/libs/intro.js/4.3.0/intro.min.js'),
+    ui.tags.script(src='https://cdnjs.cloudflare.com/ajax/libs/annyang/2.6.0/annyang.min.js'),
+    ui.tags.link(rel='stylesheet', href='https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css'),
+    ui.tags.link(rel='stylesheet', href='https://cdnjs.cloudflare.com/ajax/libs/bttn.css/0.2.4/bttn.min.css'),
+    ui.tags.script(src='https://cdnjs.cloudflare.com/ajax/libs/echarts/5.5.0/echarts.min.js'),
+    ui.tags.script(src='https://cdn.jsdelivr.net/npm/wnumb@1.2.0/wNumb.min.js'),
+    ui.tags.script(src='https://cdnjs.cloudflare.com/ajax/libs/noUiSlider/14.6.3/nouislider.min.js'),
+    ui.tags.link(rel='stylesheet', href='https://cdnjs.cloudflare.com/ajax/libs/noUiSlider/14.6.3/nouislider.min.css'),
+    ui.tags.script(src='https://unpkg.com/@popperjs/core@2/dist/umd/popper.min.js'),
+    ui.tags.script(src='https://unpkg.com/tippy.js@6/dist/tippy-bundle.umd.js'),
+    ui.tags.link(rel='stylesheet', href='https://unpkg.com/tippy.js@6/animations/scale.css'),
+    ui.include_css(css_file),
+    ui.include_js(js_file)
+  ),
+  # body (html modules)
+  ui.HTML(f"<body>{htmlContent}</body>"),
+  ui.output_ui("code"),
+  ui.output_text("code2"),
+  ui.HTML("<input id='value' min='0' max='100' value='50' type='range' oninput='Shiny.setInputValue(`value`, Number($(`#value`).val()), { priority: `event`})'> <div id='myChart' style='width: 450px; height: 500px;'></div>")
 )
 
 # server
 def server(input, output, session):
-    @output
-    @render.ui
-    def txt():
-        return ui.HTML(f'<span>The value of n*2 is <b style="color: #008cba;">{input.slider1() * 2}</b>.</span>')
+
+  @output
+  @render.text
+  def code():
+    return ui.HTML(
+    """
+    <script>
+    const myChartUse = echarts.init(document.getElementById('myChart'));
+    
+    function update1 (value1) {
+      const option = {
+        series: [{
+          type: 'gauge',
+          data: [{
+            name: 'Pressure',
+            value: value1
+          }]
+        }]
+      };
+      myChartUse.setOption(option);
+    }
+      
+    update1();
+      
+    function update2 () {
+      const value1 = Number(document.getElementById('value').value);
+      update1(value1)
+    }
+      
+    document.getElementById('value').addEventListener('input', update2);
+    </script>
+    """
+    )
+  
+  @reactive.calc
+  def reac2():
+    return (input.value() + 5) / 2
+
+  @output
+  @render.text
+  def code2():
+    return reac2()
+      
+  @reactive.effect
+  @reactive.event(input.setBttnLang)
+  def _():
+    ui.modal_show(
+      ui.modal(
+        ui.HTML(
+          """
+          <div class='gridClass'>
+          <div class='gridTwelve'>
+          <div class='gridClass'>
+          <div class='gridTwelve' style='text-align: center; font-size: 20px;'>
+          Select your preferred language:</div>
+          <br>
+          <div class='gridTwelve' style='text-align: center;'>
+          <button class='i18nBttnClass' onclick='changeLanguageAndInit(`en`);'>English</button>
+          <button class='i18nBttnClass' onclick='changeLanguageAndInit(`es`);'>Español</button>
+          <button class='i18nBttnClass' onclick='changeLanguageAndInit(`ru`);'>Русский</button>
+          </div>
+          <br>
+          <div class='gridTwelve' style='text-align: center;'>
+          <button class='i18nBttnClass' onclick='changeLanguageAndInit(`ch`);'>中国人</button>
+          <button class='i18nBttnClass' onclick='changeLanguageAndInit(`jp`);'>日本語</button>
+          <button class='i18nBttnClass' onclick='changeLanguageAndInit(`pt`);'>Portugu\u00EAs</button>
+          </div>
+          </div>
+          </div>
+          </div>
+          """
+        ),
+        title=ui.HTML(
+          """
+          <div><i class='fas fa-earth-americas' style='position: relative;
+          inset: 4px 0 0 -2px; font-size: 37px; color: #54cede;
+          text-shadow: 2px 0 #020202;'></i> Change language</div>
+          """
+        ),
+        easy_close=True,
+        footer=None
+      )
+    )
 
 app = App(app_ui, server)
 
